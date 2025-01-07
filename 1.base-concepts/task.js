9e7af27c689222824c4c84a14c1bfd86b2fc7fbc
Igor Eliseev <eliseev.igor2207@gmail.com>
2025-01-08 00:25:31 +0500

    Исправление ошибки

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -4,8 +4,8 @@ function solveEquation(a, b, c) {
 	let arr = [];
 	let D = Math.pow(b, 2) - 4 * a * c;
 	if (D > 0) {
-		arr[0] = (-b + Math.sqrt(d)) / (2 * a);
-		arr[1] = (-b - Math.sqrt(d)) / (2 * a);
+		arr[0] = (-b + Math.sqrt(D)) / (2 * a);
+		arr[1] = (-b - Math.sqrt(D)) / (2 * a);
 	} else if (D === 0) {
 		arr[0] = -b / (2 * a);
 	} else if (D < 0) {
