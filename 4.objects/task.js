75b5fdd2519d3d12e0417c2c5945e31f24445df0
Igor Eliseev <eliseev.igor2207@gmail.com>
2025-01-09 02:46:29 +0500

    Четвёртое задание выполнено

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,19 +1,35 @@
 function Student(name, gender, age) {
-  
+	this.name = name;
+	this.gender = gender;
+	this.age = age;
+	this.marks = [];
 }
 
-Student.prototype.setSubject = function (subjectName) {
-  
+Student.prototype.setSubject = function(subjectName) {
+	this.subject = subjectName;
 }
 
-Student.prototype.addMarks = function (...marks) {
-  
+Student.prototype.addMarks = function(...marksToAdd) {
+	if (!this.marks) { //проверка на то, что оценки есть (студент не отчислен)
+		return;
+	}
+	if (!!marksToAdd.length) { //если поле не пустое, то
+		this.marks.push(...marksToAdd);
+	}
+	return;
 }
 
-Student.prototype.getAverage = function () {
-  
+
+Student.prototype.getAverage = function() {
+	if (!this.marks || !this.marks.length) { //если нет поля marks или есть, но пустое, то
+		return 0;
+	}
+	const sum = this.marks.reduce((acc, item) => acc + item); //если поле есть, то считаем average
+	return sum / this.marks.length;
 }
 
-Student.prototype.exclude = function (reason) {
-  
+Student.prototype.exclude = function(reason) { //функция, которая принимает параметр причина отчисления
+	delete this.subject;
+	delete this.marks;
+	this.excluded = reason;
 }
