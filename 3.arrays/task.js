6f2b6e1053b93b6cf1a481d2b4f5613d70b19584
Igor Eliseev <eliseev.igor2207@gmail.com>
2025-01-08 19:44:47 +0500

    Третье задание выполнено

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,7 +1,27 @@
 function compareArrays(arr1, arr2) {
-  
+	if (arr1.length !== arr2.length) {
+		return false;
+	}
+
+	return arr1.every((value, index) => {
+		return value === arr2[index];
+	});
 }
 
 function getUsersNamesInAgeRange(users, gender) {
-  
+	const filteredUsers = users.filter(user => user.gender === gender); //метод для получения нужных пользователей
+
+	if (filteredUsers.length === 0) {
+		return 0;
+	}
+
+	//Формирование среднего значения, используется reduce:
+	const sumAge = filteredUsers.reduce((acc, user) => acc + user.age, 0);
+	const averageAge = sumAge / filteredUsers.length;
+	return averageAge;
+
+	//Вариант, когда отдельно формируется массив только возрастов через map, а далее среднее через reduce:
+	/*const ages = filteredUsers.map(user => user.age)
+	const averageAge = (ages.reduce((sum, age) => (sum + age)))/filteredUsers.length;
+	return averageAge;*/
 }
